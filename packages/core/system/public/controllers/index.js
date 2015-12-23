59bed3fba2f90b9a11587f68c11946c7c9bf0bc8
Josh Silverman <joshs.silverman@gmail.com>
2015-12-23 13:46:54 -0500

    good state

--- packages/core/system/public/controllers/index.js
+++ packages/core/system/public/controllers/index.js
@@ -6,48 +6,42 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
     $scope.sites = {
       'photos':{
         'name':'Photos & Places',
-        'text':'Photos & Places.',
-        'author':'Josh Silverman',
         'image':'/system/assets/img/travel2.jpg'
       },
       'me':{
         'name':'Me',
-        'text':'Me',
-        'author':'Josh Silverman',
         'image':'/system/assets/img/me2.jpg'
       },
       'wisr':{
         'name':'Wisr.com',
-        'text':'Wisr.com',
-        'author':'Zendo Labs, Inc.',
         'image':'/system/assets/img/wisr2.png'
       },
       'books':{
         'name':'Books',
-        'text':'Books',
-        'author':'Various',
         'image':'/system/assets/img/books.jpg'
+      },
+      'interests':{
+        'name':'Interests',
+        'image':'/system/assets/img/timeline.png'
+      },
+      'travel':{
+        'name':'Travel',
+        'image':'/system/assets/img/map.gif'
       }
     };
     $scope.packages = {
       'gmap':{
         'name':'gmap',
-        'text':'gmap lets you add geographical information to your applications objects',
-        'author':'linnovate',
         'link':'http://www.qed42.com',
         'image':'/theme/assets/img/gmap.png'
       },
       'upload':{
         'name':'Upload',
-        'text':'hello text',
-        'author':'Linnovate',
         'link':'http://www.linnovate.net',
         'image':'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
       },
       'socket':{
         'name':'Socket',
-        'text':'Socket.io support',
-        'author':'Linnovate',
         'link':'http://www.linnovate.net',
         'image':'http://cdn.designbyhumans.com/pictures/blog/09-2013/pop-culture-cats/Pop_Culture_Cats_Hamilton_Hipster.jpg'
       }
